c020b4190a765f99cb0ee1c77e4d3a533e2a5285
Fa Antal <67446075+zemberitenyezo@users.noreply.github.com>
2021-01-18 08:43:12 +0100

    Comments' formatting

--- Distributed Loaded Dice Algorithm/distributed_loaded_dice_algorithm_in_English.py
+++ Distributed Loaded Dice Algorithm/distributed_loaded_dice_algorithm_in_English.py
@@ -1,4 +1,4 @@
-#The decentralized loaded dice algorithm's executable proof of concept Python script.
+# The decentralized loaded dice algorithm's executable proof of concept Python script.
 
 import hashlib
 
@@ -7,24 +7,24 @@ import hashlib
 # 'joker' is a simple integer
 def distributed_loaded_dice(candidates, joker):
     previous_hash = 0
-    #looping through all candidate names one by one
+    # looping through all candidate names one by one
     for candidate in candidates:
-        #creating the chance counter for each candidate
+        # creating the chance counter for each candidate
         for chance in range(candidate[1]):
-            #concatenating ("adding") a particular value of the chance counter to the candidate's name
-            #as well as a commonly known real world random number that changes with time, like a lottery
+            # concatenating ("adding") a particular value of the chance counter to the candidate's name
+            # as well as a commonly known real world random number that changes with time, like a lottery
             s = candidate[0]+str(chance)+str(joker)
-            #calculating the pseudo random ("hash") function
+            # calculating the pseudo random ("hash") function
             next_hash = hashlib.sha256(s.encode('utf-8'))
-            #added integer conversion for compatibility
+            # added integer conversion for compatibility
             next_hash = int.from_bytes(next_hash.digest(), byteorder='big', signed=False)
-            #looking for a hash greater than any previously calculated one
+            # looking for a hash greater than any previously calculated one
             if next_hash > previous_hash:
-                #if a candidate has a hash value (relating to the card's value in the card game example)
-                #higher than any previvously checked, then this candidate's name is temporarily stored
-                #as suggestion for after the algorithm will have finished
+                # if a candidate has a hash value (relating to the card's value in the card game example)
+                # higher than any previvously checked, then this candidate's name is temporarily stored
+                # as suggestion for after the algorithm will have finished
                 suggested = candidate[0]
-                #as well as the hash value is also stored to compare later hash values against while the algorithm is running
+                # as well as the hash value is also stored to compare later hash values against while the algorithm is running
                 previous_hash = next_hash
     return(suggested)
 
