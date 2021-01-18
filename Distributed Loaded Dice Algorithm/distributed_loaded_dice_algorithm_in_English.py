89b6503167dd4f8ae908d13d6ea9a55e075eb2f7
Fa Antal <67446075+zemberitenyezo@users.noreply.github.com>
2021-01-18 07:45:19 +0100

    Add files via upload

---
+++ Distributed Loaded Dice Algorithm/distributed_loaded_dice_algorithm_in_English.py
@@ -0,0 +1,50 @@
+#The decentralized loaded dice algorithm's executable proof of concept Python script.
+
+import hashlib
+
+def distributed_loaded_dice(candidates, joker):
+    previous_hash = 0
+    #looping through all candidate names one by one
+    for candidate in candidates:
+        #creating the chance counter for each candidate
+        for chance in range(candidate[1]):
+            #concatenating ("adding") a particular value of the chance counter to the candidate's name
+            #as well as a commonly known real world random number that changes with time, like a lottery
+            s = candidate[0]+str(chance)+str(joker)
+            #calculating the pseudo random ("hash") function
+            next_hash = hashlib.sha256(s.encode('utf-8'))
+            #added integer conversion for compatibility
+            next_hash = int.from_bytes(next_hash.digest(), byteorder='big', signed=False)
+            #looking for a hash greater than any previously calculated one
+            if next_hash > previous_hash:
+                #if a candidate has a hash value (relating to the card's value in the card game example)
+                #higher than any previvously checked, then this candidate's name is temporarily stored
+                #as suggestion for after the algorithm will have finished
+                suggested = candidate[0]
+                #as well as the hash value is also stored to compare later hash values against while the algorithm is running
+                previous_hash = next_hash
+    return(suggested)
+
+ 
+print("How many oppositional candidates run in your local district?")
+number_of_candidates = int(input())
+print()
+
+candidates_list = []
+# collecting candidates' data
+for i in range(number_of_candidates):
+    # f i.th 
+    print("What is the name of candidate number", i+1, "?")
+    its_name = input()
+    # popularity of the i.th candidate's party
+    print("Please enter the popularity of the party that candidate number", i+1, "belongs to:")
+    its_chance = int(float(input())*100)
+    # extend list of locally runing candidates
+    candidates_list.append([its_name, its_chance])
+    print()
+
+print("Please add the publicly widely agreed upon random number, like a lottery, that has been randomly generated closely before election:") # Refer to README for details.
+common_real_life_random_number = int(input())
+print()
+
+print("Please cast your vote to the following local candidate in your voting district:", distributed_loaded_dice(candidates_list, common_real_life_random_number))
